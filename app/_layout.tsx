e575ec4cba315b44c52bddbd3d4adf4fcc390852
Jwilv <juan.ignacio.wilvers@gmail.com>
2024-03-02 18:46:51 -0300

    base config

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,22 @@
+import { Platform, SafeAreaView, StatusBar, StyleSheet } from 'react-native'
+import { Slot } from 'expo-router';
+
 import '../global.css';
 
-import { Stack } from 'expo-router';
 
 export default function Layout() {
-  return <Stack />;
+  return (
+    <SafeAreaView
+      style={styles.androidSafeArea}
+      className='w-full h-full'
+    >
+      <Slot />
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  androidSafeArea: {
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+  },
+})
